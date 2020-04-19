87823543f2ce6fa23e4652dab00bce32d2db2136
96StanleyLee <96stanleylee@gmail.com>
2020-04-19 12:22:25 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,31 @@
 // Code DigitalClicker Component Here
+import React from 'react'
+
+export default class DigitalClicker extends React.Component {
+
+
+    constructor(){
+        super()
+        this.state = {
+            timesClicked: 0
+        }
+    }
+
+    clickButton = () =>{
+        this.setState(previousState =>{
+            return{
+                timesClicked : previousState.timesClicked + 1
+            }
+        })
+    }
+
+
+    render(){
+      return <button type="button"
+      onClick = {this.clickButton}
+      >{this.state.timesClicked}</button>
+
+    }
+
+
+  }
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,60 @@
 // Code YouTubeDebugger Component Here
+import React from 'react'
+
+export default class YoutubeDebugger extends React.Component{
+
+
+
+    constructor(){
+        super()
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                resolution: '1080p'
+                }
+            }
+        }
+    }
+
+    handleBitRate = () =>{
+        this.setState({
+            settings: {
+               ...this.state.settings,
+               bitrate: 12
+            }
+        })
+    }
+
+    handleResolution = () =>{
+        this.setState({
+            settings: {
+                ... this.state.settings,
+               video:{
+                   resolution: '720p'
+               }
+            }
+        })
+
+        console.log(this.state)
+
+    }
+
+
+    
+
+    render() {
+        return (
+          <div>
+            <button className="bitrate" onClick = {this.handleBitRate} >
+              Change bitrate
+            </button>
+            <button className="resolution" onClick = {this.handleResolution} >
+              Change resolution
+            </button>
+          </div>
+        );
+      }
+}
